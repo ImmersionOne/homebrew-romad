590fb7e34de7ce8e25cdfb71a33390eb9dfdbde6
ImmersionOne <axum202@gmail.com>
2026-03-19 20:13:47 -0700

    fix: update SHA after bugfix

--- Formula/romad.rb
+++ Formula/romad.rb
@@ -4,7 +4,7 @@ class Romad < Formula
   desc "Travel networking toolkit for digital nomads"
   homepage "https://github.com/ImmersionOne/romad"
   url "https://github.com/ImmersionOne/romad/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "d78b7d7bcdf973694c454fffca34cceca50dfb857e836340ec17ba8b0be348db"
+  sha256 "465cf34a935b7f2e58b3c1c2675b86543c663a37dee96f22e145cb962bb0b343"
   license "MIT"
 
   depends_on "python@3.12"
